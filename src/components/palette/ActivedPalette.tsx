897cd86169361b7ddfbe0149c30f656da2884801
PKyes44 <rugate23@gmail.com>
2025-02-14 17:22:20 +0900

    chore: remove not used buttons

--- src/components/palette/ActivedPalette.tsx
+++ src/components/palette/ActivedPalette.tsx
@@ -1,11 +1,9 @@
 import { IoClose } from "react-icons/io5";
 import { PaletteType } from "../../type/palette.type";
 import ChartsPalette from "./palettes/Chart/ChartsPalette";
-import DesignPalette from "./palettes/DesignPalette";
 import DrawPalette from "./palettes/DrawPalette";
 import ElementsPalette from "./palettes/ElementsPalette";
 import TextPalette from "./palettes/TextPalette";
-import UploadsPalette from "./palettes/UploadsPalette";
 
 interface ActivedPalette {
   activeType: PaletteType;
@@ -17,9 +15,6 @@ function ActivedPalette({ activeType, inactivePalette }: ActivedPalette) {
     let targetPalette = null;
 
     switch (activeType) {
-      case "Design":
-        targetPalette = <DesignPalette />;
-        break;
       case "Elements":
         targetPalette = <ElementsPalette />;
         break;
@@ -31,9 +26,6 @@ function ActivedPalette({ activeType, inactivePalette }: ActivedPalette) {
         break;
       case "Charts":
         targetPalette = <ChartsPalette />;
-        break;
-      case "Uploads":
-        targetPalette = <UploadsPalette />;
         break;
     }
 
